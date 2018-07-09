efc10e88600cd5a08e09db868d31000d84aafaa8
clare0125 <clare.woozie1@gmail.com>
2018-07-09 20:51:00 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,9 +1,8 @@
+puts "yaint"
 
-# Challenge 1: Fill this string in with your favorite word
+your_favorite_word = "yaint"
 
-your_favorite_word = ""
-
-puts "My favorite word is #{your_favorite_word}"
+puts "My favorite word is yaint"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
